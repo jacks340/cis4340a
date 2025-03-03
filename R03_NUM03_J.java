ca44135bdbd9c407a3462746dcd9af7139c80389
jacks340 <jackmabal@protonmail.com>
2025-03-02 20:21:44 -0500

    Adding vulnerabilities
    java files of the 5 mandatory vulnerabilities

---
+++ R03_NUM03_J.java
@@ -0,0 +1,7 @@
+public class R03_NUM03_J {
+
+    public static int getInteger(DataInputStream is) throws IOException {
+        return is.readInt(); 
+    }
+
+}
---
+++ R04_STR03_J.java
@@ -0,0 +1,9 @@
+public class R04_STR03_J {
+
+    BigInteger x = new BigInteger("530500452766");
+    byte[] byteArray = x.toByteArray();
+    String s = new String(byteArray);
+    byteArray = s.getBytes();
+    x = new BigInteger(byteArray);
+
+}
